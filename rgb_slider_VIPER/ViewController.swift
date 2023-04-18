41ace3e87faa538679844b35e445aa6c3fccd06a
Christian.Starly <yoh.chrisitan@gmail.com>
2023-04-18 15:02:53 -0400

    Initial Commit

---
+++ rgb_slider_VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rgb_slider_VIPER
+//
+//  Created by Christian Calixto on 18/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
